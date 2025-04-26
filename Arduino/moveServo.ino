342977bb8dac7a5a9feea396eb08a8ec05f1700c
Gladwin Ferdz Del Rosario <gladelrosario12@gmai.com>
2025-04-26 16:23:10 +0800

    fix: reading integer by Arduino

--- Arduino/moveServo.ino
+++ Arduino/moveServo.ino
@@ -9,36 +9,34 @@ void setup() {
     Serial.begin(9600);
     servo_8.attach(8, 500, 2500);
     servo_9.attach(9, 500, 2500);
-  }
+}
   
 void loop() {
-    if (Serial.available() > 0) {
-      int signal = Serial.parseInt();  // read integer sent from Python
-      if (signal == 1) { // Biodegradable
-        for(pos1 = 5; pos1 <= 150; pos1++) {
-            servo_8.write(pos1);
-            delay(20);
+    if (Serial.available()) {
+        char incomingChar = Serial.read();  // Read one character
+        int signal = incomingChar - '0';    // Convert '1' (ASCII) -> 1 (int)
+
+        if (signal == 1) { // Biodegradable
+            for (pos1 = 5; pos1 <= 150; pos1++) {
+                servo_8.write(pos1);
+                delay(20);
+            }
+            for (pos1 = 5; pos1 >= 150; pos1--) {
+                servo_8.write(pos1);
+                delay(20);
+            }
+            delay(500);
+
+        } else if (signal == 2) { // Non-Biodegradable
+            for (pos = 5; pos <= 140; pos++) {
+                servo_9.write(pos);
+                delay(20);
+            }
+            for (pos = 5; pos >= 140; pos--) {
+                servo_9.write(pos);
+                delay(20);
+            }
+            delay(700);
         }
-
-
-        for(pos1 = 5; pos1 <= 150; pos1--) {
-            servo_8.write(pos1);
-            delay(20);
-        }
-
-        delay(500);
-      } else if (signal == 2) { // Non-Biodegradable
-        for(pos = 5; pos <= 140; pos++) {
-            servo_9.write(pos);
-            delay(20);
-        }
-
-        for(pos = 5; pos <= 140; pos--) {
-            servo_9.write(pos);
-            delay(20);
-        }
-
-        delay(700);
-      }
     }
-  }
+}
