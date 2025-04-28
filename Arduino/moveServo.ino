b1d3ffd23e26488442a54fe0c74f1709c6c2f53e
Gladwin Ferdz Del Rosario <gladelrosario12@gmai.com>
2025-04-28 15:51:59 +0800

    fix: updated values in Arduino

--- Arduino/moveServo.ino
+++ Arduino/moveServo.ino
@@ -1,7 +1,7 @@
 #include <Servo.h>
 
-int pos = 150;
-int pos1 = 150;
+int pos = 5;
+int pos1 = 5;
 Servo servo_8;
 Servo servo_9;
 
@@ -9,31 +9,31 @@ void setup() {
     Serial.begin(9600);
     servo_8.attach(8, 500, 2500);
     servo_9.attach(9, 500, 2500);
-<<<<<<< Updated upstream
 }
-  
+
+// loop code
 void loop() {
     if (Serial.available()) {
         char incomingChar = Serial.read();  // Read one character
         int signal = incomingChar - '0';    // Convert '1' (ASCII) -> 1 (int)
 
         if (signal == 1) { // Biodegradable
-            for (pos1 = 150; pos1 >= 5; pos1++) {
+            for (pos1 = 5; pos1 <= 150; pos1++) {
                 servo_8.write(pos1);
                 delay(20);
             }
-            for (pos1 = 150; pos1 <= 5; pos1--) {
+            for (pos1 = 5; pos1 >= 150; pos1--) {
                 servo_8.write(pos1);
                 delay(20);
             }
             delay(500);
 
         } else if (signal == 2) { // Non-Biodegradable
-            for (pos = 150; pos >= 5; pos++) {
+            for (pos = 5; pos <= 150; pos++) {
                 servo_9.write(pos);
                 delay(20);
             }
-            for (pos = 150; pos >= 5; pos--) {
+            for (pos = 5; pos >= 150; pos--) {
                 servo_9.write(pos);
                 delay(20);
             }
@@ -41,38 +41,3 @@ void loop() {
         }
     }
 }
-=======
-  }
-  
-void loop() {
-    if (Serial.available() > 0) {
-      int signal = Serial.parseInt();  // read integer sent from Python
-      if (signal == 1) { // Biodegradable
-        for(pos1 = 5; pos1 <= 150; pos1++) {
-            servo_8.write(pos1);
-            delay(20);
-        }
-
-
-        for(pos1 = 5; pos1 <= 150; pos1--) {
-            servo_8.write(pos1);
-            delay(20);
-        }
-
-        delay(500);
-      } else if (signal == 2) { // Non-Biodegradable
-        for(pos = 5; pos <= 140; pos++) {
-            servo_9.write(pos);
-            delay(20);
-        }
-
-        for(pos = 5; pos <= 140; pos--) {
-            servo_9.write(pos);
-            delay(20);
-        }
-
-        delay(700);
-      }
-    }
-  }
->>>>>>> Stashed changes
